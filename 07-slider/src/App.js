c8ad4a5228fc7d1e82012b5a5c0e2f5262f321a3
rahul975337 <tilwani03@gmail.com>
2020-11-09 00:54:28 +0530

    slider done

--- 07-slider/src/App.js
+++ 07-slider/src/App.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 function App() {
   const [people, setPeople] = useState(data);
-  const [index, setIndex] = React.useState(0);
-
+  const [index, setIndex] = useState(0);
+  // /////////////////////////////
   useEffect(() => {
     const lastIndex = people.length - 1;
     if (index < 0) {
@@ -15,16 +15,12 @@ function App() {
       setIndex(0);
     }
   }, [index, people]);
-
   useEffect(() => {
     let slider = setInterval(() => {
       setIndex(index + 1);
-    }, 5000);
-    return () => {
-      clearInterval(slider);
-    };
+    }, 3000);
+    return () => clearInterval(slider);
   }, [index]);
-
   return (
     <section className="section">
       <div className="title">
@@ -35,24 +31,22 @@ function App() {
       <div className="section-center">
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
-
-          let position = 'nextSlide';
+          let position = "nextSlide";
           if (personIndex === index) {
-            position = 'activeSlide';
+            position = "activeSlide";
           }
           if (
             personIndex === index - 1 ||
             (index === 0 && personIndex === people.length - 1)
           ) {
-            position = 'lastSlide';
+            position = "lastSlide";
           }
-
           return (
             <article className={position} key={id}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
-              <p className="text">{quote}</p>
+              <p className="quote">{quote}</p>
               <FaQuoteRight className="icon" />
             </article>
           );
