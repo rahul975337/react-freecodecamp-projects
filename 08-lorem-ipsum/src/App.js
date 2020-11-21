190ee08f66d1260a0e4ccc928e81fa5997e5ff34
rahul975337 <tilwani03@gmail.com>
2020-11-21 22:37:22 +0530

    done

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
@@ -13,23 +13,26 @@ function App() {
     if (count > 8) {
       amount = 8;
     }
-    setText(data.slice(0, amount));
+    setText(data.slice(0, count));
   };
   return (
-    <section className='section-center'>
-      <h3>tired of boring lorem ipsum?</h3>
-      <form className='lorem-form' onSubmit={handleSubmit}>
-        <label htmlFor='amount'>paragraphs:</label>
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum ?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
         <input
-          type='number'
-          name='amount'
-          id='amount'
+          type="number"
+          name="amount"
+          placeholder="0"
+          id="amount"
           value={count}
           onChange={(e) => setCount(e.target.value)}
         />
-        <button className='btn'>generate</button>
+        <button className="btn" onClick={handleSubmit}>
+          generate
+        </button>
       </form>
-      <article className='lorem-text'>
+      <article className="lorem-text">
         {text.map((item, index) => {
           return <p key={index}>{item}</p>;
         })}
